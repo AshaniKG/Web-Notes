9882901265b005b156f39d83ec8d0c3bf2f2782a
KGAMadushika <ashanim683@gmail.com>
2024-06-24 20:21:06 +0530

    Add files via upload

--- WebNotes/src/java/controllers/DisplayController.java
+++ WebNotes/src/java/controllers/DisplayController.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import model.notesFetchModel;
 
 /**
@@ -32,11 +33,11 @@ public class DisplayController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        // Sandun Sign In unama UserId Eka Session ekata dala methanin eka Catch karanna oni
         
-       //        HttpSession session = request.getSession(true);
-       //        String userId = session.getAttribute("userId").toString();
-           String userId = "2";
+        
+               HttpSession session = request.getSession(true);
+               String userId = session.getAttribute("userId").toString();
+           
         
         try{
                try (ResultSet result = notesFetchModel.fetchNotes(userId))
